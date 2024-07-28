99d437c1c366735421ab1cebab0c98d3dfd0ee5a
Niharika Rakhewar <111850896+NiyaRakhewar@users.noreply.github.com>
2024-07-28 20:56:23 +0530

    add tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,13 @@ export const Header = () =>{
 const onlineStatus = useOnlineStatus()
   const [loginFlag, setLoginFlag] = useState(true)
     return (
-      <div className='header'>
+      <div className="flex justify-between" >
   <div>
-    <img className="logo" src={LOGO_URL} />
+    <img className="w-28" src={LOGO_URL} />
   </div>
-  <div className='nav-items'>
-    <ul>
-      <li>Online Status {onlineStatus ?"🟢":"🔴" }</li>
+  <div >
+    <ul className='flex m-5'>
+      <li className=''>Online Status {onlineStatus ?"🟢":"🔴" }</li>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/about">About</Link></li>
       <li><Link to="/contact">Contact Us</Link></li>
